719d86142164528c16626727dc0486a797951235
Valifer13 <vianviszello@gmail.com>
2026-05-12 23:52:53 +0800

    modify: modify landing page

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -12,7 +12,7 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     layout: (name) => {
         switch (true) {
-            case name === 'welcome':
+            case name === 'welcome' || name === 'landing-page':
                 return null;
             case name.startsWith('auth/'):
                 return AuthLayout;
